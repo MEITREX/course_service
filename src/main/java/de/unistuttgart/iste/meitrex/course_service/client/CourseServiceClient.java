02bcd889988eb4da06794f5fdecbdea03573e6d1
Aaron Schmid <st166450@stud.uni-stuttgart.de>
2025-06-25 00:42:13 +0200

    remove unused exception

--- src/main/java/de/unistuttgart/iste/meitrex/course_service/client/CourseServiceClient.java
+++ src/main/java/de/unistuttgart/iste/meitrex/course_service/client/CourseServiceClient.java
@@ -1,7 +1,6 @@
 package de.unistuttgart.iste.meitrex.course_service.client;
 
 
-import de.unistuttgart.iste.meitrex.course_service.exception.CourseServiceConnectionException;
 import de.unistuttgart.iste.meitrex.generated.dto.Chapter;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -29,26 +28,11 @@ public class CourseServiceClient {
         this.graphQlClient = graphQlClient;
     }
 
-    public List<Chapter> queryChapterByCourseId(final UUID courseId) throws CourseServiceConnectionException {
+    public List<Chapter> queryChapterByCourseId(final UUID courseId){
         log.info("queryContentByCourseId {}", courseId);
-        try {
-            return graphQlClient.document(QueryDefinitions.CHAPTERS_BY_COURSEID)
-                    .variable("courseId", courseId)
-                    .retrieveSync(QueryDefinitions.CHAPTERS_BY_COURSEID_QUERY_NAME)
-                    .toEntityList(Chapter.class);
-        } catch (final RuntimeException e) {
-            unwrapCourseServiceConnectionException(e);
-        }
-        return List.of(); // unreachable
+        return graphQlClient.document(QueryDefinitions.CHAPTERS_BY_COURSEID)
+                .variable("courseId", courseId)
+                .retrieveSync(QueryDefinitions.CHAPTERS_BY_COURSEID_QUERY_NAME)
+                .toEntityList(Chapter.class);
     }
-
-    private static void unwrapCourseServiceConnectionException(final RuntimeException e) throws CourseServiceConnectionException {
-        // block wraps exceptions in a RuntimeException, so we need to unwrap them
-        if (e.getCause() instanceof final CourseServiceConnectionException courseServiceConnectionException) {
-            throw courseServiceConnectionException;
-        }
-        // if the exception is not a CourseServiceConnectionException, we don't know how to handle it
-        throw e;
-    }
-
 }
